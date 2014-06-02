56716fc2564ad1e4b53e53e4be3614653a4b47b4
billytianlan <billylan22@gmail.com>
2014-06-02 21:19:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenPotatoes::Application.config.secret_key_base = 'c5c0ccc6243ee6e05e2e08775b37678981c97ef267803ecdc00b9ef17cded41ec2387ff48f4aef13ca75bf6f824a9a21ec7d64b7dc3a41de9eeab3317e73b47a'
