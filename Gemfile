979513a3c7bd166734b95d21140336e7c9f9ec6f
billytianlan <billylan22@gmail.com>
2014-06-03 18:57:38 +0000

    added photo uploader and thumbnails

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'carrierwave'
 
+gem 'mini_magick'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,9 @@ class Movie < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :title, :director, :description, :poster_image_url, :release_date, presence: true
+  mount_uploader :image, ImageUploader
+
+  validates :title, :director, :description, :image, :release_date, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validate :release_date_is_in_the_future
 
