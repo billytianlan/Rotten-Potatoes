d91377ba603c67d55c5890a14f3b55c67e58b29c
billytianlan <billylan22@gmail.com>
2014-06-03 21:12:23 +0000

    added admin controller and admin to users

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,3 @@
+Admin::UsersController < ActionController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ RottenPotatoes::Application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :admin do
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
